1d54c5f38198e12ba012cd4930ffef9fcb618049
XINSHENG2381 <90528845+XINSHENG2381@users.noreply.github.com>
2022-02-22 20:14:44 -0500

    addedBST

---
+++ binarySearchTree.py
@@ -0,0 +1,48 @@
+class BinarySearchTreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self,data):
+        if data == self.data:
+            return
+
+        if data < self.data:
+            if self.left: #检查left侧是否有node
+                self.left.add_child(data) #递归
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def in_order_traversal(self):
+        elements = []
+
+        # visit left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        # visit root node
+        elements.append(self.data)
+
+        #visit right node
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+if __name__ == '__main__':
+    numbers = [17,4,1,20,9,23,18,34]
+    numbers_tree = build_tree(numbers)
+    print(numbers_tree.in_order_traversal())  
